a5022ec1f8e2ad12b4f3cdb0a947210f69c1cede
Austin Browne <aus.bro22@gmail.com>
2018-02-26 17:25:44 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array.unshift(element)
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = array.push(element)
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
