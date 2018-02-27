d242855ea8a4f7d3cc7390c9f00b6af986a3a2d9
Austin Browne <aus.bro22@gmail.com>
2018-02-27 20:19:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function removeElementFromBeginningOfArray(array) {
   return newArray
 }
 function removeElementFromEndOfArray(array) {
-var newArray = array.slice(2);
+var newArray = array.slice(array.length-1);
 return newArray;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
