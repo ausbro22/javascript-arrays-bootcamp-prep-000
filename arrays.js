9f79f02886092655abc0d40128a7157b78aaf995
Austin Browne <aus.bro22@gmail.com>
2018-02-27 18:48:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
   return array
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  array.push(element)
   return array
 }
 function addElementToEndOfArray(array, element){
