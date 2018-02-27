96456cd0fa480846c2c3104eadcac0de2319a5d7
Austin Browne <aus.bro22@gmail.com>
2018-02-27 20:14:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToEndOfArray(array, element){
   return newArray
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
+  array.slice(-2)
   return array
 }
 function removeElementFromEndOfArray(array) {
